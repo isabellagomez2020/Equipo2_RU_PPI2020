0d5d047cbd2ee2876f6faeb473b262771c5cf6b5
Felipe Palacio <66035015+FelipePala0724@users.noreply.github.com>
2020-11-10 16:44:41 -0500

    Servicio de mostrar todos los usuarios

--- routes/Usuarios.js
+++ routes/Usuarios.js
@@ -4,13 +4,13 @@ const mysqlConnection =require('../db/db');
 
 // Colocar los middlewares  
 router.get('/usuario',(req,res)=>{
-    mysqlConnection.query('SELECT * FROM Producto',(err,rows,fiels)=>{
+    mysqlConnection.query('SELECT * FROM Usuario',(err,rows,fiels)=>{
     if(!err){
        res.json(rows); 
     }else{
     console.log(err);
     }});
     })// fin
-
+// Por probar a ver
 
 module.exports =router;
